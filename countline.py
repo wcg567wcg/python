0803d851a7988bfaccfd7e6c5b98fa12785131ff
luozhaohui <luo.zhaohui@kortide.com>
2016-07-28 18:09:26 +0800

    udpate codecs.

--- countline.py
+++ countline.py
@@ -6,19 +6,20 @@
 
 import os
 import sys
+import codecs
 
 def process_file(path):
     total = 0
-    if (path.endswith('.java')):
-        handle = open(path, 'r')
+    if (path.endswith('.java') or path.endswith('.py')):
+        handle = codecs.open(path, 'r', 'utf-8')
         for eachLine in handle:
+            print(eachLine)
+            eachLine = eachLine.lstrip()
             if len(eachLine) > 1:
-                #print len(eachLine), ' > ', eachLine,
-                eachLine = eachLine.lstrip()
                 if (eachLine.startswith("//") == False):
                     total += 1
         handle.close()
-        print path, ',', total, 'lines.'
+        #print("%s has %d lines"%(path, total))
     return total
 
 def process_dir(path):
@@ -43,8 +44,8 @@ def process(path):
     elif(os.path.isfile(path)):
         total = process_file(path)
 
-    print '>>> total lines :', total, '.'
+    print('>>> total lines : %d'%total)
     return total
 
-#process('/home/kesalin/test/test/AccountPreference.java')
-process('/home/kesalin/test/Settings')
+process('/home/luozhaohui/test.py')
+#process('/home/kesalin/test/Settings')
